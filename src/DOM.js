0768f3546cefa2e30382aa99943a5e1c16e862c9
Raymond Zou <raymond.1zou@gmail.com>
2022-11-04 23:40:52 -0700

    add day of the week to weather cards

--- src/DOM.js
+++ src/DOM.js
@@ -1,4 +1,5 @@
 import * as weatherAPI from "./weatherAPI.js";
+import { add, format } from 'date-fns';
 
 const form = document.getElementById("submit-form");
 const locationInput = document.querySelector('#submit-form input');
@@ -13,19 +14,22 @@ form.onsubmit = function(event) {
 async function renderWeather() {
     clearCards();
     const weatherData = await weatherAPI.getWeeklyForecast(locationInput.value);
-    console.log({weatherData});
+    var day = new Date();
+    console.log(day);
     header.textContent = weatherData[0].weatherDay.name;
     for (let i=0; i<weatherData.length; i++) {
         const weatherDay = weatherData[i].weatherDay;
-        createCard(weatherDay.temp_main, weatherDay.desc, weatherDay.icon_code);
+        createCard(weatherDay.temp_main, weatherDay.desc, weatherDay.icon_code, format(day, 'ccc'));
+        day = add(day, {days: 1});
     }
 }
 
-function createCard(temperature, description, iconCode) {
+function createCard(temperature, description, iconCode, dayofWeek) {
     const weatherCard = document.createElement('div');
     const temp = document.createElement('div');
     const desc = document.createElement('div');
     const icon = document.createElement('img');
+    const date = document.createElement('div');
     weatherCard.classList.add('weather-card')
     temp.classList.add('card-temp');
     desc.classList.add('card-desc');
@@ -33,6 +37,8 @@ function createCard(temperature, description, iconCode) {
     temp.textContent = `${temperature}°C`;
     desc.textContent = description;
     icon.src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    date.textContent = dayofWeek;
+    weatherCard.append(date);
     weatherCard.append(icon);
     weatherCard.append(temp);
     weatherCard.append(desc);
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -3,12 +3,10 @@ async function getWeeklyForecast(location) {
     try {
         const cityCoords = await getCoords(location);
         const weatherDays = [];
-        const cityName = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${cityCoords[0].lat}&lon=${cityCoords[0].lon}&limit=1&appid=af89899d870ab5f02a09bfa241e01353`)
+            const cityName = await fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${cityCoords[0].lat}&lon=${cityCoords[0].lon}&limit=1&appid=af89899d870ab5f02a09bfa241e01353`)
                                 .then(data => data.json()).then(data => data[0].name)
         const data = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityCoords[0].lat}&lon=${cityCoords[0].lon}&cnt=7&appid=af89899d870ab5f02a09bfa241e01353`);
         const JSONdata = await data.json();
-        console.log(JSONdata.daily[0].weather[0].description);
-        console.log(cityName);
         for (let i = 0; i < 7; i++) {
 
             const desc = JSONdata.daily[i].weather[0].description;
