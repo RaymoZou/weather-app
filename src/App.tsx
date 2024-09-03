ece873d0503e3be5cd7c8fe78713d90a942aa054
RaymoZou <raymond.1zou@gmail.com>
2024-09-02 22:47:28 -0700

    add querying for temperature, humidity and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+// weather data structure for a given City
+type City = {
+    name: string, // name of the city
+    temperature: number, // in Kelvin
+    humidity: number, // in 
+    description: string
+}
+
+// placeholder for weather data
+// const Vancouver: City = {
+//     name: "Vancouver",
+//     temperature: 12,
+//     humidity: 13,
+//     description: "always raining..."
+// }
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const [city, setCity] = useState<City | null>(null)
+    const [query, setQuery] = useState<string>("");
+
+    // make API call for 1-day weather forecast for city using Current Weather Data API
+    async function fetchData() {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${import.meta.env.VITE_API_KEY}`)
+
+        if (!response.ok) {
+            // TODO: handle error
+            return
+        }
+
+        var data = await response.json();
+        setCity({
+            name: data.name,
+            temperature: data.main.temp,
+            humidity: data.main.humidity,
+            description: data.weather[0].description
+        })
+    }
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setQuery(event.target.value);
+    }
+
+    return (
+        <>
+            <h1>name: {city?.name}</h1>
+            <p>temp: {city?.temperature}</p>
+            <p>humidity: {city?.humidity}</p>
+            <p>desc: {city?.description}</p>
+            <input
+                onChange={handleChange}
+                placeholder="City Name"
+            />
+            <button
+                onClick={fetchData}
+                className="btn btn-primary">
+                Get Weather
+            </button>
+        </>
+    )
 }
 
 export default App
